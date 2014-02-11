8b75cb013e404fe0c24384795b00463edd4c49d2
Jonathan Kemp <kempdogg@gmail.com>
2014-02-11 00:05:18 -0500

    Refactor using bind.

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
+'use strict';
+
 var gutil = require('gulp-util');
 var through = require('through2');
 var juice = require('juice');
 
 module.exports = function(opt){
     return through.obj(function (file, enc, cb) {
-        var self = this;
-
         if (!opt) {
             opt = {};
         }
 
         if (file.isNull()) {
-            self.push(file);
+            this.push(file);
             return cb();
         }
 
         if (file.isStream()) {
-            self.emit('error', new gutil.PluginError('gulp-inline-css', 'Streaming not supported'));
+            this.emit('error', new gutil.PluginError('gulp-inline-css', 'Streaming not supported'));
             return cb();
         }
 
         juice(file.path, opt, function (err, html) {
             if (err) {
-                self.emit('error', new gutil.PluginError('gulp-inline-css', err));
+                this.emit('error', new gutil.PluginError('gulp-inline-css', err));
             }
 
             file.contents = new Buffer(String(html));
 
-            self.push(file);
+            this.push(file);
 
             return cb();
-        });
+        }.bind(this));
     });
 };
