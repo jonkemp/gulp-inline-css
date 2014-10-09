ccc42fb0b595b9f6821fd79f230fa8ddba83d02f
Zach Rose <zach@zachrose.biz>
2014-10-09 09:20:44 -0700

    Add test to demonstrate issue #13

--- test/main.js
+++ test/main.js
@@ -7,15 +7,18 @@ var path = require('path');
 var gutil = require('gulp-util');
 var inlineCss = require('../index');
 
-function compare(input, options, done) {
+function compare(inputPath, outputPath, options, done) {
+    
+    var input = fs.readFileSync(inputPath);
+    
     var fakeFile = new gutil.File({
-        path: path.resolve('./test/fixtures/in.html'),
+        path: path.resolve('./'+inputPath),
         cwd: './test/',
         base: './test/fixtures/',
         contents: new Buffer(String(input))
     });
 
-    var output = fs.readFileSync(path.join('test', 'expected', 'out.html'));
+    var output = fs.readFileSync(outputPath);
 
     // Create a plugin stream
     var stream = inlineCss(options);
@@ -36,8 +39,18 @@ function compare(input, options, done) {
 
 describe('gulp-inline-css', function() {
     it('Should convert linked css to inline css', function(done) {
-        var input = fs.readFileSync(path.join('test', 'fixtures', 'in.html'));
+        var inputPath = path.join('test', 'fixtures', 'in.html');
+        var outputPath = path.join('test', 'expected', 'out.html');
         var options = {};
-        compare(input, options, done);
+        compare(inputPath, outputPath, options, done);
+    });
+    it('Should handle multiple HTML files with relative stylesheets', function(done){
+        var one = path.join('test', 'fixtures', 'multiple', 'one', 'in.html');
+        var two = path.join('test', 'fixtures', 'multiple', 'two', 'in.html');
+        var options = {};
+        var doneCount = 0;
+        var bothDone = function(){ doneCount++; if(doneCount == 2){ done(); } };
+        compare(one, path.join('test', 'expected', 'multiple', 'one', 'out.html'), options, bothDone);
+        compare(two, path.join('test', 'expected', 'multiple', 'two', 'out.html'), options, bothDone);
     });
 });
