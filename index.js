f20f3918cfec3cab27dcf99c8b5066a7344eb382
Kemp, Jonathan <jkemp@scrippsnetworks.com>
2014-10-30 13:13:29 -0400

    Fix issue with relative stylesheets across files.
    Fixes #13.

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 'use strict';
-var gutil = require('gulp-util');
-var through = require('through2');
-var juice = require('juice2');
+
+var gutil = require('gulp-util'),
+    through = require('through2'),
+    juice = require('juice2');
 
 module.exports = function(opt){
     return through.obj(function (file, enc, cb) {
-        opt = opt || {};
+        var _opt = JSON.parse(JSON.stringify(opt || {}));
 
         // 'url' option is required
         // set it automatically if not provided
-        opt.url = opt.url || 'file://' + file.path;
+        if (!_opt.url) {
+            _opt.url = 'file://' + file.path;
+        }
 
         if (file.isStream()) {
             this.emit('error', new gutil.PluginError('gulp-inline-css', 'Streaming not supported'));
             return cb();
         }
 
-        juice.juiceContent(file.contents, opt, function(err, html) {
+        juice.juiceContent(file.contents, _opt, function(err, html) {
             if (err) {
                 this.emit('error', new gutil.PluginError('gulp-inline-css', err));
             }
