834e2a031e0feb4108a77cb616e3256c04c04d6a
Kemp, Jonathan <jkemp@scrippsnetworks.com>
2014-10-30 13:14:58 -0400

    Add tests for relative stylesheets across files.

--- test/main.js
+++ test/main.js
@@ -1,27 +1,29 @@
 /* jshint node: true */
 /* global describe, it */
 
-var assert = require('assert');
-var fs = require('fs');
-var path = require('path');
-var gutil = require('gulp-util');
-var inlineCss = require('../index');
+'use strict';
 
-function compare(input, options, done) {
-    var fakeFile = new gutil.File({
-        path: path.resolve('./test/fixtures/in.html'),
+var assert = require('assert'),
+    fs = require('fs'),
+    path = require('path'),
+    gutil = require('gulp-util'),
+    inlineCss = require('../index');
+
+function getFile(filePath) {
+    return new gutil.File({
+        path: path.resolve(filePath),
         cwd: './test/',
-        base: './test/fixtures/',
-        contents: new Buffer(String(input))
+        base: path.dirname(filePath),
+        contents: new Buffer(String(fs.readFileSync(filePath)))
     });
+}
 
-    var output = fs.readFileSync(path.join('test', 'expected', 'out.html'));
-
+function compare(fixturePath, expectedPath, options, done) {
     // Create a plugin stream
     var stream = inlineCss(options);
 
     // write the fake file to it
-    stream.write(fakeFile);
+    stream.write(getFile(fixturePath));
 
     // wait for the file to come back out
     stream.once('data', function (file) {
@@ -29,15 +31,20 @@ function compare(input, options, done) {
         assert(file.isBuffer());
 
         // check the contents
-        assert.equal(file.contents.toString('utf8'), String(output));
+        assert.equal(file.contents.toString('utf8'), String(fs.readFileSync(expectedPath)));
         done();
     });
 }
 
 describe('gulp-inline-css', function() {
     it('Should convert linked css to inline css', function(done) {
-        var input = fs.readFileSync(path.join('test', 'fixtures', 'in.html'));
         var options = {};
-        compare(input, options, done);
+        compare(path.join('test', 'fixtures', 'in.html'), path.join('test', 'expected', 'out.html'), options, done);
+    });
+
+    it('Should inline css in multiple HTML files', function(done) {
+        var options = {};
+        compare(path.join('test', 'fixtures', 'multiple', 'one', 'in.html'), path.join('test', 'expected', 'multiple', 'one', 'out.html'), options, function () {});
+        compare(path.join('test', 'fixtures', 'multiple', 'two', 'in.html'), path.join('test', 'expected', 'multiple', 'two', 'out.html'), options, done);
     });
 });
